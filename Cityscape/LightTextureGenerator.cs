82a1ae637b39a0c9c0787cb14216db3f5c072ea8
Chris <Chris@Pokey>
2009-06-16 19:53:04 +0100

    Light texture and a bit of refactoring
    --HG--
    extra : convert_revision : chris%40pokey-20090616185304-ob77v0r3v2aqq4zj

---
+++ Cityscape/LightTextureGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace Cityscape
+{
+    class LightTextureGenerator
+    {
+        public const int textureWidth = 256;
+        public const int textureHeight = 256;
+        private static Texture2D texture_= null;
+        private static IGraphicsDeviceService deviceService_ = null;
+
+        public static IGraphicsDeviceService deviceService
+        {
+            set
+            {
+                deviceService_ = value;
+            }
+        }
+
+        public static Texture2D texture
+        {
+            get
+            {
+                if (texture_ == null && deviceService_ != null)
+                {
+                    texture_ = new Texture2D(deviceService_.GraphicsDevice, textureWidth, textureHeight, 0, TextureUsage.AutoGenerateMipMap, SurfaceFormat.Color);
+                    Color[] textureData = new Color[textureWidth * textureHeight];
+                    texture_.GetData<Color>(textureData);
+                    for (int x = 0; x < textureWidth; x++)
+                        for (int y = 0; y < textureHeight; y++)
+                        {
+                            float xDist = (float)(x - textureWidth / 2);
+                            float yDist = (float)(y - textureHeight / 2);
+                            xDist /= (float)(textureWidth / 2);
+                            yDist /= (float)(textureHeight / 2);
+                            float dist = (float)Math.Sqrt(xDist * xDist + yDist * yDist);
+                            textureData[x + y * textureWidth] = new Color(1.0f, 1.0f, 1.0f, 1.0f-dist);
+                        }
+                    texture_.SetData(textureData);
+                    texture_.Save("lighttexture_.png", ImageFileFormat.Png);
+                }
+                return texture_;
+            }
+        }
+    }
+}
